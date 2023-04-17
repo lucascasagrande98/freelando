ef070e4ba7181e1be5a25121b7ed7ba175bc1c33
lucascasagrande98 <lucascasagrande98@gmail.com>
2023-04-17 11:55:13 -0300

    criando componentes Tipografia, Cabecalho, Rodape e CampoTexto

---
+++ src/componentes/Rodape/Rodape.jsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Rodape = styled.footer`
+	background: ${(props) => props.theme.cores.primarias.a};
+	padding: ${(props) => props.theme.espacamento.xl};
+	color: ${(props) => props.theme.cores.branco};
+`;
